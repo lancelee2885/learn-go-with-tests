4bfd03bc20d653932b020e51263c5d99fedc8dfe
Lance Lee <lancelee@Lances-MacBook-Pro.local>
2022-07-20 20:31:53 -0700

    folder rearrangement

--- hello_word/hello_test.go
+++ hello_word/hello_test.go
@@ -12,15 +12,29 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Lance")
+		got := Hello("Lance", "")
 		want := "Hello, Lance!"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World!"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie!"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Jack", "French")
+		want := "Bonjour, Jack!"
 
 		assertCorrectMessage(t, got, want)
 	})
